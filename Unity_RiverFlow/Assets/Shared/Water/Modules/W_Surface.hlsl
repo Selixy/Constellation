16a243d95450e90614b552686bc352a40c1ec1a6
Selixy <selixy@users.noreply.github.com>
2026-04-10 00:49:36 +0200

    c'est un peut beau

--- Unity_RiverFlow/Assets/Shared/Water/Modules/W_Surface.hlsl
+++ Unity_RiverFlow/Assets/Shared/Water/Modules/W_Surface.hlsl
@@ -2,34 +2,34 @@
 #define W_SURFACE_INCLUDED
 
 TEXTURE2D(_WaterFlowMap); SAMPLER(sampler_WaterFlowMap);
-float4 _WaterFlowMap_TexelSize;
-float4 _WaterPlaneSize;
+float4 _WaterFlowMap_TexelSize;  
+float4 _WaterPlaneSize;          
 
 half4 ComputeWaterSurface(float2 uv)
 {
-    float2 d = _WaterFlowMap_TexelSize.xy * 2.5;
+    // On récupère la taille d'un pixel et on l'agrandit pour le rayon du blur
+    float2 d = _WaterFlowMap_TexelSize.xy * 2.5; // On tape à côté pour adoucir
 
-    float h_C  = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv).z;
-    float h_L  = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2(-d.x,  0   )).z;
-    float h_R  = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2( d.x,  0   )).z;
-    float h_D  = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2( 0,   -d.y )).z;
-    float h_U  = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2( 0,    d.y )).z;
-    float h_DL = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2(-d.x, -d.y )).z;
-    float h_DR = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2( d.x, -d.y )).z;
-    float h_UL = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2(-d.x,  d.y )).z;
-    float h_UR = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2( d.x,  d.y )).z;
+    // Blur 3x3 avec un filtre quasi-Gaussien (efface l'effet on/off aliasing)
+    float h_C = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv).z;
+    float h_L = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2(-d.x, 0)).z;
+    float h_R = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2( d.x, 0)).z;
+    float h_D = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2(0, -d.y)).z;
+    float h_U = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2(0,  d.y)).z;
 
-    float blurred = (h_C * 4.0 + (h_L + h_R + h_D + h_U) * 2.0 + (h_DL + h_DR + h_UL + h_UR)) / 16.0;
+    float h_DL = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2(-d.x, -d.y)).z;
+    float h_DR = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2( d.x, -d.y)).z;
+    float h_UL = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2(-d.x,  d.y)).z;
+    float h_UR = SAMPLE_TEXTURE2D(_WaterFlowMap, sampler_WaterFlowMap, uv + float2( d.x,  d.y)).z;
 
-    // Normale de surface depuis le gradient de hauteur
-    float gx = h_R - h_L;
-    float gy = h_U - h_D;
-    float3 normal = normalize(float3(-gx, -gy, 0.1));
+    // Moyenne pondérée lissée (adoucissement du grain / bruit de la simulation)
+    float blurred_height = (h_C * 4.0 + (h_L + h_R + h_D + h_U) * 2.0 + (h_DL + h_DR + h_UL + h_UR) * 1.0) / 16.0;
 
-    // Encode en couleur RGB [0,1]
-    float3 col = normal * 0.5 + 0.5;
-    float  alpha = saturate(abs(blurred) * 4.0);
-    return half4(col, alpha);
+    // Image 100% blanche, alpha = hauteur lissée
+    float alpha = blurred_height;
+    alpha -= 0.5; // On recentre autour de 0.5 pour que les pixels à hauteur nulle soient à moitié transparents (au lieu d'invisibles)
+
+    return half4(1.0, 1.0, 1.0, alpha);
 }
 
 #endif
